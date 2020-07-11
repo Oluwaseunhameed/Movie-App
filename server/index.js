4bb7643968fce86083eeb72406cc4ab0013a41c4
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-07-11 13:52:41 +0100

    heroku deployment commands

--- server/index.js
+++ server/index.js
@@ -22,15 +22,12 @@ app.use("/api/favorite", require("./routes/favorite"));
 
 app.use("/uploads", express.static("uploads"));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  // index.html for all page routes
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static('client/build'))
+  const path = require('path')
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 const port = process.env.PORT || 5000;
