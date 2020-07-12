92b048d4090d6c5032958b753cde4173dba865a0
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-07-12 09:31:27 +0100

    changes to config folder

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const connectDB = require('./config/db')
 const path = require('path');
-
-const app = express();
-
-connectDB()
-
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+const app = express();
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,13 +23,15 @@ app.use("/api/favorite", require("./routes/favorite"));
 
 app.use("/uploads", express.static("uploads"));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
